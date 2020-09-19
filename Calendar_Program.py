ce9423d967d1e7939088a6e367f0229daf5aa802
Shaurya2525 <70660632+Shaurya2525@users.noreply.github.com>
2020-09-19 14:28:50 -0700

    Create Calendar_Program.py

---
+++ Calendar_Program.py
@@ -0,0 +1,4 @@
+print("Here is my schedule for Monday and Tuesday")
+print("Day               Time               Subject")
+print("Monday            9:10 - 10:15 AM    Science")
+print("Tuesday           10:35 - 11:40 AM   Pf")
